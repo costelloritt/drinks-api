ced7175ac0a08f073f0d946687c17ba30d3f2a24
Unknown <costello.ritt@gmail.com>
2017-11-11 16:40:49 -0600

    add bundle structure with router and data model

--- router.js
+++ router.js
@@ -1,7 +1,10 @@
 const routes = require('express').Router();
+const inventory = require('./bundles/inventory-bundle/settings/router-config');
 
 routes.get('/', (req, res) => {
     res.status(200).send('This is the home route.');
 });
+
+routes.use('/inventory', inventory);
 
 module.exports = routes;

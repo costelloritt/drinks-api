eba18fbfa66a3b1a2505de87e17ef588edf525e5
Unknown <costello.ritt@gmail.com>
2017-11-12 13:21:45 -0600

    add multiple routes within index controller

--- bundles/inventory-bundle/bundle-config/router-config.js
+++ bundles/inventory-bundle/bundle-config/router-config.js
@@ -1,6 +1,7 @@
 const routes = require('express').Router();
 const indexController = require('../controllers/indexController');
 
-routes.get('/', indexController);
+routes.get('/type', indexController.view_inventory_by_type);
+routes.get('/brand', indexController.view_inventory_by_brand);
 
 module.exports = routes;
--- bundles/inventory-bundle/controllers/indexController.js
+++ bundles/inventory-bundle/controllers/indexController.js
@@ -1,6 +1,11 @@
 const data = require('../models/inv-data.json');
 
-module.exports = (req, res) => {
+exports.view_inventory_by_type = (req, res) => {
     const drinkType = data.drinkType;
     res.status(200).json({ drinkType });
+};
+
+exports.view_inventory_by_brand = (req, res) => {
+    const drinkBrand = data.drinkBrand;
+    res.status(200).json({ drinkBrand });
 };

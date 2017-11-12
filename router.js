0b07c69f85b8f1dd78e373fe7153ac088cf6397f
Unknown <costello.ritt@gmail.com>
2017-11-12 13:25:47 -0600

    rename bundle config and router

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 const routes = require('express').Router();
-const inventory = require('./bundles/inventory-bundle/bundle-config/router-config');
+const inventory = require('./bundles/inventory-bundle/config/router');
 
 routes.get('/', (req, res) => {
     res.status(200).send('This is the home route.');
